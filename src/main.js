b371a17228c1e5e340ef81c45795ab227997fce7
ZHGqiancheng <604080903@qq.com>
2019-10-25 21:52:14 +0800

    使用饿了么UI库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// ElementUI 是一个插件  集成了很多组件
+Vue.use(ElementUI) // 全局使用
 Vue.config.productionTip = false
-
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+// main.js的作用
+// 职责1:创建一个根实例
+// 职责2:复杂全局范围的依赖导入
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router' // 引入vue-router
+import Vue from 'vue'
+Vue.use(VueRouter) // 全局使用
+const router = new VueRouter({
+  // 路由配置对象
+  // 关键选项:routes 作用 配置路由规则
+  routes: [
+
+  ]
+})
+export default router // 导出 需要在main.js导入
